79e474533561913194019630ce1cd54220e53aa7
nisino25 <nisino25@gmail.com>
2022-11-20 19:53:01 +0900

    hey

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 <head>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="apple-mobile-web-app-capable" content="yes"></meta>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
 
